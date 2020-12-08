3a7612fc63b82b791942c8424ac45c88c1246e3a
Harsh Motwani <harshmotwanimail@gmail.com>
2020-12-08 15:08:22 +0530

    Added semaphores and set up message processing thread

--- src/com/squids/MessageListeningThread.java
+++ src/com/squids/MessageListeningThread.java
@@ -37,12 +37,14 @@ public class MessageListeningThread extends Thread{
                 if(message.charAt(0) == 'M'){
                     System.out.printf("Received a message: %s\n", message);
                     //Add to requests queue
+                    GlobalVariables.messageSem.acquire();
                     GlobalVariables.messageRequests.add(message);
+                    GlobalVariables.messageSem.release();
                     //Close this socket
                     socket.close();
                     in.close();
                 }
-            } catch (IOException e) {
+            } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
                 return;
             }
